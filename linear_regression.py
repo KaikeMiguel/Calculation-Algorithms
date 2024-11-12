6346cb89005a10bb3726a4fc200c803ef94b7296
Kaike Miguel <132000016+KaikeMiguel@users.noreply.github.com>
2024-11-12 11:48:43 -0300

    Finished linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -7,53 +7,74 @@ f'(x) = 7ax^6 + 6bx^5 + 5cx^4 + 4dx^3 + 3ex^2 + 2fx + g
 
 df(a) = 2*(y1 - (a1x^7 + b1x^6 + c1x^5 + d1x^4 + e1x^3 + f1x^2 g1x + h)) * (-x^7) + ... + até o final
 df(b) até df(h)
+
+# gradientes[k] += 2*(y - (coef[0]*(x**7) + coef[1]*(x**6) + coef[2]*(x**5) + coef[3]*(x**4) + coef[4]*(x**3) + coef[5]*(x**2) + coef[6]*(x) + coef[7])) * (-x**(7-k))
 '''
 
 
-dataset_x = [0, 1, 2, 3, 4, 5, 6, 7]
-dataset_y = [0, 1, 4, 9, 16, 25, 36, 49]
+dataset_x = [-0.27, 0.1, 0.42, 0.59, 1.1, 1.08, 1.16, 1.33]
+dataset_y = [6.33, 8.76, 14.0, 16.3, 36, 40.53, 58.9, 85.3]
 
 coef = [1, 1, 1, 1, 1, 1, 1, 1]
 
 
 # Gradiente Descendente
 def gds(dataset_x, dataset_y, coef):
-    gradientes = [0] * len(coef)
     n = len(coef)
+    gradientes = [0] * n
 
     for i in range(len(dataset_x)):
 
         x = dataset_x[i]
         y = dataset_y[i]
 
-        for k in range(len(coef)):
+        for k in range(n):
             soma = 0
 
-            for j in range(len(coef)):
+            for j in range(n):
                 soma += coef[j]*(x**(n-j-1))
             
-            gradientes[k] += 2 * (y - soma) * (x**(7-k))
-            
-            # gradientes[k] += 2*(y - (coef[0]*(x**7) + coef[1]*(x**6) + coef[2]*(x**5) + coef[3]*(x**4) + coef[4]*(x**3) + coef[5]*(x**2) + coef[6]*(x) + coef[7])) * (-x**(7-k))
+            gradientes[k] += 2 * (y - soma) * -(x**(7-k))
     
     return gradientes
 
 
 # Função para calcular a distância
 def distancia(coef1, coef2):
-    return sum((coef1[i] - coef[i]) ** 2 for i in range(len(coef1))) ** (1/2)
+    return sum((coef1[i] - coef2[i]) ** 2 for i in range(len(coef1))) ** (1/2)
 
 
 # Gradiente Descendente aplicado na fórmula: (Xn+1 = Xn - Lr * gds)
 def gds_formula(dataset_x, dataset_y, coef, lr, tol):
     n = len(coef)
-    Xn = [9999] * n
+    Xn = [9999] * n  # Chute Iicial
     Xn1 = coef
     
-    i = 1
+    c = 1
     dist = distancia(Xn, Xn1)
 
     while dist > tol:
 
-        for i in range(len(coef)):
-            Xn1 = coef[i]
+        grad = gds(dataset_x, dataset_y, coef)
+        Xn = Xn1 + []  # Substitui os valores de Xn para Xn+1
+
+        for i in range(n):
+            Xn1[i] = Xn[i] - lr * grad[i] / n
+
+        dist = distancia(Xn, Xn1)
+        c += 1
+
+        # teste para ver se está diminuindo ou crescendo
+        # if c % 1000 == 0:
+        #     print(c, dist, Xn1)
+    
+    return (c, Xn1)
+
+
+
+lr = 1e-6
+tol = 5e-5
+
+print("Carregando...")
+g = gds_formula(dataset_x, dataset_y, coef, lr, tol)
+print(f"f(x) = {round(g[1][0],2)}x^7 + {round(g[1][1],2)}x^6 + {round(g[1][2],2)}x^5 + {round(g[1][3],2)}x^4 + {round(g[1][4],2)}x^3 + {round(g[1][5],2)}x^2 + {round(g[1][6],2)}x + {round(g[1][7],2)}")
